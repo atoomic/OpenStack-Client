8254d9967d06f498d25bfd1d9abe3b0e80715f2c
Nicolas R <atoomic@cpan.org>
2019-04-12 10:23:57 -0500

    wip

--- lib/OpenStack/Client/Lite.pm
+++ lib/OpenStack/Client/Lite.pm
@@ -1,1 +1,86 @@
-1
+package OpenStack::Client::Lite;
+
+use strict;
+use warnings;
+
+use OpenStack::Client::Lite::Routes   ();
+
+use Moo;
+use Test::More;
+
+has 'debug'           => ( is => 'rw', default => 0 );
+has 'auth' => (
+	is      => 'ro',
+	lazy    => 1,
+	default => sub {
+		die "Auth missing...";
+	},
+	handles => [qw/version/],
+);
+
+has 'route' => (
+	is => 'ro',
+#	lazy    => 1,
+	default => sub { OpenStack::Client::Lite::Routes->new() }, 
+	handles => [ OpenStack::Client::Lite::Routes::list_all() ],
+);
+
+around BUILDARGS => sub {
+  my ( $orig, $class, @args ) = @_;
+
+  die "Missing arguments to create Auth object" unless scalar @args;
+  return { auth =>  OpenStack::Client::Auth->new( @args ) };
+};
+
+
+1;
+
+__END__
+
+
+# right now I can login using this
+
+my $auth = OpenStack::Client::Auth->new($endpoint,
+        'username' => $ENV{'OS_USERNAME'},
+        'password' => $ENV{'OS_PASSWORD'},
+        version => 3,
+        scope => {
+            project => {
+                name => $ENV{'OS_PROJECT_NAME'},
+                domain => { id => 'default' },
+            }
+        }
+    );
+ 
+# then I would need to know where to go to do my request
+
+my $api = $auth->service('compute',
+    'region' => $ENV{'OS_REGION_NAME'}
+);
+
+my @keypairs = $api->all('/os-keypairs', 'keypairs');    
+
+# my idea is to abstract this
+my $api = OpenStack::Client::Lazy->new( auth => $auth ); # or ::Easy ?
+my @keypairs = $api->keypairs();
+
+
+
+
+OpenStack::API::
+
+API.pm 
+
+keypairs => {
+    service  => 'compute',
+    listable => 1,
+}
+
+API/Compute/v2_1.pm
+
+keypairs => {
+    uri => '/os-keypairs',
+    key => 'keypairs',
+}
+
+compute => 
--- lib/OpenStack/Client/Lite/Routes.pm
+++ lib/OpenStack/Client/Lite/Routes.pm
@@ -1,1 +1,25 @@
-1
+package OpenStack::Client::Lite::Routes;
+
+use strict;
+use warnings;
+
+use Test::More;
+use Moo;
+
+our $ROUTES = {
+	keypairs => {
+	    service  => 'compute',
+	    listable => 1,
+	},
+
+};
+
+sub list_all {
+	return sort keys %$ROUTES;
+}
+
+sub keypairs {
+	note "keypairs... todo";
+}
+
+1;
